dfa1082d209287ab626314f7744e75732480d455
Cooper <cooper@glowbase.xyz>
2022-02-27 13:52:01 +1100

    move to firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,141 @@
+const { Storage } = require('@google-cloud/storage');
 const functions = require('firebase-functions');
+const { parse } = require('node-html-parser');
 const firebase = require('firebase-admin');
+const axiosRetry = require('axios-retry');
 const moment = require('moment');
 const axios = require('axios');
+const fs = require('fs');
+
+const storage = new Storage({ keyFilename: 'gcp_key.json' });
 
 require('dotenv').config();
-firebase.initializeApp();
 
+firebase.initializeApp({
+  databaseURL: "https://glowbase-portfolio-default-rtdb.asia-southeast1.firebasedatabase.app"
+});
+
+axiosRetry(axios, { retries: 50,
+  retryDelay: () => { return 0 },
+  retryCondition: error => {
+    return error.response.status.toString().startsWith('5');
+  },
+});
+
+/**
+ * 
+ * @param {*} imageUrl 
+ * @param {*} postId 
+ * @returns 
+ */
+async function downloadFile(imageUrl, postId) {
+  try {
+    const { data } = await axios({
+      method: "GET",
+      url: imageUrl,
+      responseType: "stream",
+      headers: {
+        'Origin': 'https://liveuamap.com'
+      }
+    });
+  
+    data.pipe(fs.createWriteStream(`./${postId}.jpg`));
+
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+
+/**
+ * 
+ * @param {*} postId 
+ * @returns 
+ */
+async function uploadFile(postId) {
+  await storage.bucket('russia-ukraine-news').upload(`./${postId}.jpg`, {
+    destination: `${postId}.jpg`,
+  });
+
+  return `https://storage.googleapis.com/russia-ukraine-news/${postId}.jpg`;
+}
+
+
+async function getImage(html, postId) {
+  const image = html.querySelector('img');
+
+  if (!image) return '';
+
+  const result = await downloadFile(image.getAttribute('src'), postId);
+
+  if (!result) return '';
+
+  const url = await uploadFile(postId);
+
+  fs.unlinkSync(`./${postId}`);
+
+  return url;
+}
+
+
+/**
+ * 
+ * @param {*} html 
+ * @returns 
+ */
+ function getTime(html) {
+  const time = html.querySelector('.date_add').innerText.split(' ');
+  let time_difference = time[0];
+
+  if (time[1] === 'hour') {
+    time_difference = time_difference * 3600000;
+  } else {
+    time_difference = time_difference * 60000;
+  }
+
+  return new Date().getTime() - time_difference;
+}
+
+
+/**
+ * 
+ * @returns 
+ */
+async function getPostHTML() {
+  const { data } = await axios.get('https://liveuamap.com');
+  const post_html = parse(data).querySelector('#feedler').childNodes[0];
+
+  return {
+    html: post_html,
+    id: post_html.id.replace('post-', '')
+  };
+}
+
+
+/**
+ * 
+ * @param {*} html 
+ * @returns 
+ */
+async function extractPostData(html) {
+  const id = html.id.replace('post-', '');
+  const image = await getImage(html, id);
+  const time = getTime(html);
+
+  return {
+    content: html.querySelector('.title').innerText,
+    timestamp: time,
+    image: image,
+  }
+}
+
+
+/**
+ * 
+ * @param {*} data 
+ * @param {*} webhook 
+ */
 async function postWebhook(data, webhook) {
   await axios({
     method: 'POST',
@@ -17,6 +147,10 @@ async function postWebhook(data, webhook) {
   });
 }
 
+
+/**
+ * 
+ */
 exports.sendWebhook = functions.database.ref('/posts/{postId}/').onCreate(async (snapshot, context) => {
   console.log(`Received Post ${context.params.postId}`);
 
@@ -47,6 +181,10 @@ exports.sendWebhook = functions.database.ref('/posts/{postId}/').onCreate(async 
   });
 });
 
+
+/**
+ * 
+ */
 exports.addWebhook = functions.database.ref('/webhooks/{channelId}').onCreate(async (snapshot, context) => {
   console.log(`Webhook Added ${context.params.channelId}`);
   
@@ -61,4 +199,26 @@ exports.addWebhook = functions.database.ref('/webhooks/{channelId}').onCreate(as
   }
 
   postWebhook(data, snapshot.val());
+});
+
+
+/**
+ * 
+ */
+exports.getPost = functions.pubsub.schedule('* * * * *').timeZone('Australia/Sydney').onRun(async () => {
+  const { html, id } = await getPostHTML();
+  
+  const postExists = await firebase.database().ref(`/posts/${id}`).once('value');
+
+  if (postExists.val()) return console.log('NO UPDATE');
+  
+  const post = await extractPostData(html);
+
+  await firebase.database().ref('/posts/').update({
+    [id]: post
+  });
+
+  console.log('ADDED POST:', id);
+
+  return null;
 });
