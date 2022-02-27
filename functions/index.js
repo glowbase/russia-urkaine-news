4b0ba0cc195ca70b974b8b57cdbba6024fb4b2e2
Cooper <cooper@glowbase.xyz>
2022-02-27 12:08:51 +1100

    add/send webhook functions

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,64 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
 const moment = require('moment');
 const axios = require('axios');
 
 require('dotenv').config();
+firebase.initializeApp();
 
-async function postWebhook(post, webhook) {
-  const time_formatted = moment(post.timestamp).format('h:mm A \- DD/MM/YYYY');
-
+async function postWebhook(data, webhook) {
   await axios({
     method: 'POST',
     headers: {
       "Content-Type": "application/json"
     },
     url: webhook,
-    data: JSON.stringify({
-      "content": null,
-      "embeds": [
-        {
-          "description": post.content,
-          "color": 16711680,
-          "footer": {
-            "text": "Posted at " + time_formatted
-          },
-          "image": {
-            "url": post.image
-          }
-        }
-      ]
-    })
+    data: JSON.stringify(data)
   });
 }
 
-exports.webhook = functions.database.ref('/posts/{postId}/').onCreate(async (snapshot, context) => {
+exports.sendWebhook = functions.database.ref('/posts/{postId}/').onCreate(async (snapshot, context) => {
   console.log(`Received Post ${context.params.postId}`);
 
-  postWebhook(snapshot.val(), process.env.WEBHOOK);
+  const { timestamp, content, image } = snapshot.val();
+
+  const time_formatted = moment(timestamp).format('h:mm A \- DD/MM/YYYY');
+
+  const data = {
+    "content": null,
+    "embeds": [
+      {
+        "description": content,
+        "color": 16711680,
+        "footer": {
+          "text": "Posted at " + time_formatted
+        },
+        "image": {
+          "url": image
+        }
+      }
+    ]
+  }
+
+  const webhooks = await firebase.database().ref('/webhooks/').once('value');
+
+  webhooks.forEach(webhook => {
+    postWebhook(data, webhook.val());
+  });
+});
+
+exports.addWebhook = functions.database.ref('/webhooks/{channelId}').onCreate(async (snapshot, context) => {
+  console.log(`Webhook Added ${context.params.channelId}`);
+  
+  const data = {
+    "content": null,
+    "embeds": [
+      {
+        "description": "Webhook added to system. This channel will now receive live Russia & Ukraine news.",
+        "color": 16711680,
+      }
+    ]
+  }
+
+  postWebhook(data, snapshot.val());
 });
